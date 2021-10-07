ab64960e8ca51230620a979c5469be9345b57ac1
Evgeniya Bayda <baje75128@gmail.com>
2021-10-08 00:57:33 +0300

    change class on fn

--- src/components/FeedbackOptions/FeedbackOptions.js
+++ src/components/FeedbackOptions/FeedbackOptions.js
@@ -1,32 +1,28 @@
-import { Component } from 'react';
 import s from './FeedbackOptions.module.css';
 import PropTypes from 'prop-types';
 
-export default class FeedbackOptions extends Component {
-  static defaultProps = {
-    options: [],
-  };
-  static propTypes = {
-    options: PropTypes.array.isRequired,
-    onLeaveFeedback: PropTypes.func.isRequired,
-  };
-
-  render() {
-    const { options, onLeaveFeedback } = this.props;
-    return (
-      <div className={s.allButton}>
-        {options.map((option, ind) => (
-          <button
-            className={s.button}
-            value={option}
-            key={ind}
-            type="button"
-            onClick={e => onLeaveFeedback(e)}
-          >
-            {option}
-          </button>
-        ))}
-      </div>
-    );
-  }
+export default function FeedbackOptions({ options, onLeaveFeedback }) {
+  return (
+    <div className={s.allButton}>
+      {options.map((option, ind) => (
+        <button
+          className={s.button}
+          value={option}
+          key={ind}
+          type="button"
+          onClick={e => onLeaveFeedback(e)}
+        >
+          {option}
+        </button>
+      ))}
+    </div>
+  );
 }
+
+FeedbackOptions.defaultProps = {
+  options: [],
+};
+FeedbackOptions.propTypes = {
+  options: PropTypes.array.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
